886e9d4464de828e396b9f149070fbccc3f1dab1
nandhinik17 <118190100+nandhinik17@users.noreply.github.com>
2025-03-13 17:38:17 +0530

    Create lengthStringWithoutUsingInbuiltMethod.java

---
+++ lengthStringWithoutUsingInbuiltMethod.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class lengthStringWithoutUsingInbuiltMethod {
+    public static void main(String[] args) {
+        String str = "I am a Automation Tester";
+        int len =0;
+        for(char ch: str.toCharArray()){
+            len++;
+        }
+        int i=0;
+        System.out.println(len)
+        
+    }
+}
+
