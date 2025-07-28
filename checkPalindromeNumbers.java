00cd0b02129ef48f29f315b17830229d0753c529
nandhinik17 <118190100+nandhinik17@users.noreply.github.com>
2025-07-28 13:00:03 +0530

    Create checkPalindromeNumbers.java

---
+++ checkPalindromeNumbers.java
@@ -0,0 +1,18 @@
+class checkPalindromeNumbers {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int original =x;
+        int reversed =0;
+        while(x>0){
+
+           reversed = reversed *10 + (x%10);
+            x=x/10;
+        }
+        if(original==reversed){
+            return true;
+        }
+        return false;
+    }
+}
