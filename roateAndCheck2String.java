149e70b4003130262c55c04c336112b926126772
nandhinik17 <118190100+nandhinik17@users.noreply.github.com>
2025-07-29 10:59:26 +0530

    Create roateAndCheck2String.java

---
+++ roateAndCheck2String.java
@@ -0,0 +1,21 @@
+/*Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true */
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        String s1= s+ s;
+        return s1.contains(goal);
+    }
+}
