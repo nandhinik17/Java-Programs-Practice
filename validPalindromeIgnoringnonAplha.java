8632c433de4973967549b8a692ec8bee5879ccb8
nandhinik17 <118190100+nandhinik17@users.noreply.github.com>
2025-07-30 20:34:01 +0530

    Create validPalindromeIgnoringnonAplha.java

---
+++ validPalindromeIgnoringnonAplha.java
@@ -0,0 +1,27 @@
+/* A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.*/
+
+
+class Solution {
+    public boolean isPalindrome(String s) {
+       StringBuilder sb = new StringBuilder();
+       for(char ch: s.toCharArray()){
+        if(Character.isLetterOrDigit(ch)){
+            sb.append(ch);
+        }
+       }
+
+       String normal = sb.toString().toLowerCase();
+       String reversed = sb.reverse().toString().toLowerCase();
+       return normal.equals(reversed);
+    }
+}
