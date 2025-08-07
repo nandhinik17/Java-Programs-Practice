bf3947bebab285e4d35b1d33807f22bf4b54542a
nandhinik17 <118190100+nandhinik17@users.noreply.github.com>
2025-08-07 14:25:27 +0530

    Create numberofSegmentsInString.java

---
+++ numberofSegmentsInString.java
@@ -0,0 +1,28 @@
+/*Given a string s, return the number of segments in the string.
+
+A segment is defined to be a contiguous sequence of non-space characters.
+
+ 
+
+Example 1:
+
+Input: s = "Hello, my name is John"
+Output: 5
+Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+Example 2:
+
+Input: s = "Hello"
+Output: 1
+ */
+
+
+
+class Solution {
+    public int countSegments(String s) {
+        if(s.trim().isEmpty()){
+            return 0;
+        }
+        String str[] = s.trim().split("\\s+");
+        return str.length;
+    }
+}
