585493c2a2265a7e03f74242288cbac9be9ef073
nandhinik17 <118190100+nandhinik17@users.noreply.github.com>
2025-08-12 13:25:26 +0530

    Create CountVoelStringRange.java

---
+++ CountVoelStringRange.java
@@ -0,0 +1,50 @@
+/*You are given a 0-indexed array of string words and two integers left and right.
+
+A string is called a vowel string if it starts with a vowel character and ends with a vowel character where vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+
+Return the number of vowel strings words[i] where i belongs to the inclusive range [left, right].
+
+ 
+
+Example 1:
+
+Input: words = ["are","amy","u"], left = 0, right = 2
+Output: 2
+Explanation: 
+- "are" is a vowel string because it starts with 'a' and ends with 'e'.
+- "amy" is not a vowel string because it does not end with a vowel.
+- "u" is a vowel string because it starts with 'u' and ends with 'u'.
+The number of vowel strings in the mentioned range is 2.
+Example 2:
+
+Input: words = ["hey","aeo","mu","ooo","artro"], left = 1, right = 4
+Output: 3
+Explanation: 
+- "aeo" is a vowel string because it starts with 'a' and ends with 'o'.
+- "mu" is not a vowel string because it does not start with a vowel.
+- "ooo" is a vowel string because it starts with 'o' and ends with 'o'.
+- "artro" is a vowel string because it starts with 'a' and ends with 'o'.
+The number of vowel strings in the mentioned range is 3.
+ */
+
+
+
+
+
+
+class Solution {
+    public int vowelStrings(String[] words, int left, int right) {
+        int count=0;
+
+        for(int i=left; i<=right; i++){
+            String str= words[i];
+            int l = str.length()-1;
+            if((str.charAt(0)=='a' || str.charAt(0)=='e'|| str.charAt(0)=='i' || str.charAt(0)=='o'|| str.charAt(0)=='u') && (str.charAt(l)=='a' || str.charAt(l)=='e'|| str.charAt(l)=='i' || str.charAt(l)=='o'|| str.charAt(l)=='u')  ){
+     count++;
+            }
+        }
+
+        return count;
+    }
+
+}
