444b8e2286fd6ce67bba491dae632e73b0131fc8
nandhinik17 <118190100+nandhinik17@users.noreply.github.com>
2025-10-16 12:04:11 +0530

    Update removeElementsinArrayDiff.java

--- removeElementsinArrayDiff.java
+++ removeElementsinArrayDiff.java
@@ -1,14 +1,18 @@
 import java.util.*;
 
-class removeElementsinArray {
+class removeElementsinArrayDiff {
   
   public static void main (String[] args) {
     
     int arr[] = {100, 100, 90, 90, 80, 80};
     
     int k=80;
-   List<Integer> list = new ArrayList<Integer>();
+   List<Integer> list = new ArrayList<>();
+   for(int num: arr){
+     list.add(num);
+   }
    list.removeIf(n-> n==k);
    
-   System.out.println(Arrays.toString());
+   System.out.println(Arrays.toString(list.toArray(new Integer[0])));
+}
 }
