28ab0e2f0b1c283a5e781da05a2ba56f7133ef37
nandhinik17 <118190100+nandhinik17@users.noreply.github.com>
2025-10-22 11:23:23 +0530

    Create rotateArrayUsingCollections.java

---
+++ rotateArrayUsingCollections.java
@@ -0,0 +1,29 @@
+/*Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]*/
+
+
+class rotateArrayUsingCollections {
+    public void rotate(int[] nums, int k) {
+        List<Integer> list = new ArrayList<Integer>();
+        for(int n : nums){
+        list.add(n);
+        }
+        Collections.reverse(list);
+         System.out.println(list);
+        Collections.reverse(list.subList(0, k));
+         Collections.reverse(list.subList(k, list.size()));
+        System.out.println(list);
+
+        int val =0;
+        for(int n1: nums){
+            nums[val] = list.get(val);
+            val++;
+        }
+    }
+}
