3ee74df740b3da098d8bca365ddc05a1a3cc4826
Nandhini Karuppusamy <118190100+nandhinik17@users.noreply.github.com>
2025-11-26 19:23:28 +0530

    Create PascalsTriangle.java

---
+++ PascalsTriangle.java
@@ -0,0 +1,38 @@
+/*Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+ 
+
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]*/
+
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<>();
+
+       for(int i=0; i<numRows; i++){
+       List<Integer> result = new ArrayList<>();
+        for(int j=0; j<=i; j++){
+               if(j==0 || i=='i'){
+                result.add(1);
+               }
+               else{
+                //i--> 2 j--> 1 (1 row 0 value)+ (1 row 1 value)
+                int val = list.get(i-1).get(j-1) + list.get(i-1).get(j);
+                result.add(val);
+               }
+        }
+        list.add(result);
+       }
+       return list;
+    }
+}
