f7bd4fb8d6c7ec717c50bf0cd89255fa292e0a20
Nandhini Karuppusamy <118190100+nandhinik17@users.noreply.github.com>
2025-11-30 14:18:31 +0530

    Create NumberOfStudentsUnbaletoEatLunch.java

---
+++ NumberOfStudentsUnbaletoEatLunch.java
@@ -0,0 +1,62 @@
+/*The school cafeteria offers circular and square sandwiches at lunch break, referred to by numbers 0 and 1 respectively. All students stand in a queue. Each student either prefers square or circular sandwiches.
+
+The number of sandwiches in the cafeteria is equal to the number of students. The sandwiches are placed in a stack. At each step:
+
+If the student at the front of the queue prefers the sandwich on the top of the stack, they will take it and leave the queue.
+Otherwise, they will leave it and go to the queue's end.
+This continues until none of the queue students want to take the top sandwich and are thus unable to eat.
+
+You are given two integer arrays students and sandwiches where sandwiches[i] is the type of the i​​​​​​th sandwich in the stack (i = 0 is the top of the stack) and students[j] is the preference of the j​​​​​​th student in the initial queue (j = 0 is the front of the queue). Return the number of students that are unable to eat.
+
+ 
+
+Example 1:
+
+Input: students = [1,1,0,0], sandwiches = [0,1,0,1]
+Output: 0 
+Explanation:
+- Front student leaves the top sandwich and returns to the end of the line making students = [1,0,0,1].
+- Front student leaves the top sandwich and returns to the end of the line making students = [0,0,1,1].
+- Front student takes the top sandwich and leaves the line making students = [0,1,1] and sandwiches = [1,0,1].
+- Front student leaves the top sandwich and returns to the end of the line making students = [1,1,0].
+- Front student takes the top sandwich and leaves the line making students = [1,0] and sandwiches = [0,1].
+- Front student leaves the top sandwich and returns to the end of the line making students = [0,1].
+- Front student takes the top sandwich and leaves the line making students = [1] and sandwiches = [1].
+- Front student takes the top sandwich and leaves the line making students = [] and sandwiches = [].
+Hence all students are able to eat.
+Example 2:
+
+Input: students = [1,1,1,0,0,1], sandwiches = [1,0,0,0,1,1]
+Output: 3
+ */
+
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> s = new LinkedList<>();
+         int count =0;
+        for(int n: students){
+            q.offer(n);
+        }
+        for(int n1: sandwiches){
+            s.offer(n1);
+        }
+
+        while(!q.isEmpty()){
+            if(q.peek()==s.peek()){
+                q.poll();
+                s.poll();
+                count=0;
+            }
+            else{
+                q.offer(q.poll());
+                count++;
+            }
+            if(count==q.size()){
+                break;
+            }
+        }
+
+        return q.size();
+    }
+}
