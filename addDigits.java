6ec77ea4cf1a8a58cffde8c60a0fbf31c975283e
Nandhini Karuppusamy <118190100+nandhinik17@users.noreply.github.com>
2026-02-01 21:20:14 +0530

    Add addDigits method to calculate digital root
    Implement addDigits method to compute the digital root of a number.

---
+++ addDigits.java
@@ -0,0 +1,23 @@
+/*Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0*/
+
+class Solution {
+    public int addDigits(int num) {
+        if(num==0){
+            return 0;
+        }
+        
+        return 1+ (num-1)%9;
+    }
+}
+ 
